f71330b8e8d9a601a9482f48efd899d3ca3aa23a
serramatutu <lucasvvop@hotmail.com>
2025-04-13 00:52:01 +0200

    Add tokenizer test harness
    This commit adds a `snap.zig` file which contains a `SnapshotTestSession`
    struct. This struct is responsible for reading test snapshots from the
    `snap/` folder one by one, based on the file extension. The caller can
    then do whatever it wants with the test input and submit its result back
    to the session, which will write it to `test-out` and assert the result
    equals some expected output file.

---
+++ src/snap.zig
@@ -0,0 +1,117 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+const FixedBufStr = @import("fixed_buf_str.zig").FixedBufStr;
+
+/// A snapshot testing session.
+///
+/// Contains utilities for iterating over all possible snapshots and writing
+/// results to the output.
+pub const SnapshotTestSession = struct {
+    pub const SNAP_PATH = "snap";
+    pub const OUT_PATH = "test-out";
+
+    /// String max lengths
+    pub const NAME_CAP = 256;
+    pub const INPUT_CAP = 65536;
+    pub const EXPECT_CAP = 65536;
+    pub const _TMP_STR_CAP = 1024;
+
+    name: FixedBufStr,
+    input: FixedBufStr,
+    expect: FixedBufStr,
+
+    _tmp_str: FixedBufStr,
+    dir_walker: std.fs.Dir.Walker,
+    ext: []const u8,
+
+    pub fn init(a: Allocator, ext: []const u8) Allocator.Error!SnapshotTestSession {
+        const snap_dir = std.fs.cwd().openDir(SNAP_PATH, .{ .iterate = true }) catch |err| @panic(@errorName(err));
+
+        var tmp_str = try FixedBufStr.init(a, _TMP_STR_CAP);
+        errdefer tmp_str.deinit();
+
+        var name = try FixedBufStr.init(a, NAME_CAP);
+        errdefer name.deinit();
+
+        var input = try FixedBufStr.init(a, INPUT_CAP);
+        errdefer input.deinit();
+
+        var expect = try FixedBufStr.init(a, EXPECT_CAP);
+        errdefer expect.deinit();
+
+        var walker = try snap_dir.walk(a);
+        errdefer walker.deinit();
+
+        return SnapshotTestSession{
+            ._tmp_str = tmp_str,
+            .name = name,
+            .input = input,
+            .expect = expect,
+            .dir_walker = walker,
+            .ext = ext,
+        };
+    }
+
+    pub fn deinit(self: *SnapshotTestSession) void {
+        self.name.deinit();
+        self.input.deinit();
+        self.expect.deinit();
+        self.dir_walker.deinit();
+        self._tmp_str.deinit();
+    }
+
+    pub const IterError = FixedBufStr.WriteError || FixedBufStr.LoadFileError || std.fs.File.OpenError;
+
+    pub fn next(self: *SnapshotTestSession) IterError!?[]const u8 {
+        const cwd = std.fs.cwd();
+        while (self.dir_walker.next() catch |err| @panic(@errorName(err))) |entry| {
+            if (entry.kind != std.fs.Dir.Entry.Kind.file) {
+                continue;
+            }
+
+            const py_path = entry.path;
+
+            if (!std.mem.eql(u8, py_path[py_path.len - 3 .. py_path.len], ".py")) {
+                continue;
+            }
+
+            const name = py_path[0 .. py_path.len - 3];
+            _ = try self.name.load(name);
+
+            _ = try self._tmp_str.loadMany(&.{ SNAP_PATH, "/", py_path });
+            const py_file = try cwd.openFile(self._tmp_str.slice(), .{});
+            defer py_file.close();
+            _ = try self._tmp_str.loadFile(py_file);
+            _ = try self.input.loadStr(self._tmp_str);
+
+            _ = try self._tmp_str.loadMany(&.{ SNAP_PATH, "/", name, ".", self.ext });
+            const snap_file = try cwd.openFile(self._tmp_str.slice(), .{});
+            defer snap_file.close();
+            _ = try self._tmp_str.loadFile(snap_file);
+            _ = try self.expect.loadStr(self._tmp_str);
+
+            return self.input.slice();
+        }
+
+        return null;
+    }
+
+    /// Write the result of the latest run for a snapshot to the corresponding out file then assert
+    /// it is what we expect from the pristine snapshot file.
+    pub fn submitResult(self: *SnapshotTestSession, result: []const u8) !void {
+        const cwd = std.fs.cwd();
+
+        _ = try self._tmp_str.loadMany(&.{ OUT_PATH, "/", self.name.slice(), ".", self.ext });
+        const f = cwd.openFile(self._tmp_str.slice(), .{ .mode = .write_only }) catch |err|
+            switch (err) {
+                error.FileNotFound => try cwd.createFile(self._tmp_str.slice(), .{}),
+                else => return err,
+            };
+        defer f.close();
+
+        _ = try f.writer().writeAll(result);
+
+        try std.testing.expectEqualSlices(u8, self.expect.slice(), result);
+    }
+};
--- src/token.zig
+++ src/token.zig
@@ -1,17 +1,18 @@
 //! Python tokenizer
+//! See reference: https://docs.python.org/3/reference/lexical_analysis.html
 const std = @import("std");
 const assert = std.debug.assert;
-
-pub const TokenizerError = error{
-    Eof,
-};
+const FixedBufStr = @import("fixed_buf_str.zig").FixedBufStr;
+const SnapshotTestSession = @import("snap.zig").SnapshotTestSession;
 
 /// Helper to consume chars from a string
 const TextCursor = struct {
     text: []const u8,
     pos: u8,
 
-    fn new(text: []const u8) TextCursor {
+    pub const Error = error{Eof};
+
+    fn init(text: []const u8) TextCursor {
         return TextCursor{
             .text = text,
             .pos = 0,
@@ -19,25 +20,25 @@ const TextCursor = struct {
     }
 
     /// get the current token
-    inline fn curr(self: *TextCursor) TokenizerError!u8 {
+    inline fn curr(self: *TextCursor) Error!u8 {
         if (self.eof()) {
-            return TokenizerError.Eof;
+            return Error.Eof;
         }
         return self.text[self.pos];
     }
 
     /// consume a char and return it
-    fn next(self: *TextCursor) TokenizerError!u8 {
+    fn next(self: *TextCursor) Error!u8 {
         const pos = self.pos;
         try self.advance();
         return self.text[pos];
     }
 
     /// consume a char
-    fn advance(self: *TextCursor) TokenizerError!void {
+    fn advance(self: *TextCursor) Error!void {
         assert(self.pos <= self.text.len);
         if (self.eof()) {
-            return TokenizerError.Eof;
+            return Error.Eof;
         }
         self.pos += 1;
     }
@@ -56,103 +57,223 @@ const TextCursor = struct {
 };
 
 test "cursor next" {
-    var cur = TextCursor.new("abc");
+    var cur = TextCursor.init("abc");
     try std.testing.expectEqual('a', cur.next());
     try std.testing.expectEqual('b', cur.next());
     try std.testing.expectEqual('c', cur.next());
-    try std.testing.expectEqual(TokenizerError.Eof, cur.next());
+    try std.testing.expectEqual(TextCursor.Error.Eof, cur.next());
 }
-
-pub const TokenKind = enum {
-    kw_and,
-    kw_as,
-    kw_assert,
-    kw_async,
-    kw_await,
-    kw_break,
-    kw_class,
-    kw_continue,
-    kw_def,
-    kw_del,
-    kw_elif,
-    kw_else,
-    kw_except,
-    kw_false,
-    kw_finally,
-    kw_for,
-    kw_from,
-    kw_global,
-    kw_if,
-    kw_import,
-    kw_in,
-    kw_is,
-    kw_lambda,
-    kw_none,
-    kw_nonlocal,
-    kw_not,
-    kw_or,
-    kw_pass,
-    kw_raise,
-    kw_return,
-    kw_true,
-    kw_try,
-    kw_while,
-    kw_with,
-    kw_yield,
-
-    ws_newline,
-    ws_tab,
-    ws_space,
-
-    identifier,
-    nonsense,
-
-    fn keyword(self: TokenKind) []const u8 {
-        return switch (self) {
-            .kw_and => "and",
-            .kw_as => "as",
-            .kw_assert => "assert",
-            .kw_async => "async",
-            .kw_await => "await",
-            .kw_break => "break",
-            .kw_class => "class",
-            .kw_continue => "continue",
-            .kw_def => "def",
-            .kw_del => "del",
-            .kw_elif => "elif",
-            .kw_else => "else",
-            .kw_except => "except",
-            .kw_false => "False",
-            .kw_finally => "finally",
-            .kw_for => "for",
-            .kw_from => "from",
-            .kw_global => "global",
-            .kw_if => "if",
-            .kw_import => "import",
-            .kw_in => "in",
-            .kw_is => "is",
-            .kw_lambda => "lambda",
-            .kw_none => "None",
-            .kw_nonlocal => "nonlocal",
-            .kw_not => "not",
-            .kw_or => "or",
-            .kw_pass => "pass",
-            .kw_raise => "raise",
-            .kw_return => "return",
-            .kw_true => "True",
-            .kw_try => "try",
-            .kw_while => "while",
-            .kw_with => "with",
-            .kw_yield => "yield",
-            else => "",
-        };
-    }
-};
 
 pub const Token = struct {
     start: u32,
     len: u32,
-    kind: TokenKind,
+    kind: Kind,
+
+    pub const Kind = enum {
+        // keywords
+        kw_and,
+        kw_as,
+        kw_assert,
+        kw_async,
+        kw_await,
+        kw_break,
+        kw_class,
+        kw_continue,
+        kw_def,
+        kw_del,
+        kw_elif,
+        kw_else,
+        kw_except,
+        kw_false,
+        kw_finally,
+        kw_for,
+        kw_from,
+        kw_global,
+        kw_if,
+        kw_import,
+        kw_in,
+        kw_is,
+        kw_lambda,
+        kw_none,
+        kw_nonlocal,
+        kw_not,
+        kw_or,
+        kw_pass,
+        kw_raise,
+        kw_return,
+        kw_true,
+        kw_try,
+        kw_while,
+        kw_with,
+        kw_yield,
+
+        // user-defined
+        /// # a python comment
+        ud_comment,
+        /// an identifier like a variable name, module name or function name
+        ud_identifier,
+        /// any nonsense the user writes that does not match any other token type
+        ud_nonsense,
+
+        // symbols
+        /// &
+        sm_ampersand,
+        /// &=
+        sm_ampersandeq,
+        /// ->
+        sm_arrow,
+        /// @
+        sm_at,
+        /// @=
+        sm_ateq,
+        /// :
+        sm_colon,
+        /// :=
+        sm_coloneq,
+        /// ,
+        sm_comma,
+        /// .
+        sm_dot,
+        /// ...
+        sm_ellipsis,
+        /// =
+        sm_eq,
+        /// !
+        sm_exclamation,
+        /// >
+        sm_gt,
+        /// >=
+        sm_gte,
+        /// ^
+        sm_hat,
+        /// ^=
+        sm_hateq,
+        /// {
+        sm_lbrace,
+        /// [
+        sm_lbrack,
+        /// <
+        sm_le,
+        /// )
+        sm_lparen,
+        /// <<
+        sm_lshift,
+        /// <<=
+        sm_lshifteq,
+        /// <=
+        sm_lte,
+        /// -
+        sm_minus,
+        /// -=
+        sm_minuseq,
+        /// !=
+        sm_neq,
+        /// %
+        sm_percent,
+        /// %=
+        sm_percenteq,
+        /// |
+        sm_pipe,
+        /// |=
+        sm_pipeeq,
+        /// +
+        sm_plus,
+        /// +=
+        sm_pluseq,
+        /// }
+        sm_rbrace,
+        /// ]
+        sm_rbrack,
+        /// )
+        sm_rparen,
+        /// >>
+        sm_rshift,
+        /// >>=
+        sm_rshifteq,
+        /// ;
+        sm_semicolon,
+        /// /
+        sm_slash,
+        /// /=
+        sm_slasheq,
+        /// *
+        sm_star,
+        /// *=
+        sm_stareq,
+        /// ~
+        sm_tilde,
+        /// ==
+        sm_twoeq,
+        /// //
+        sm_twoslash,
+        /// //=
+        sm_twoslasheq,
+        /// **
+        sm_twostar,
+        /// **=
+        sm_twostareq,
+
+        // whitespace
+        /// \
+        ws_linejoin,
+        /// \n, \r\n or \r
+        ws_newline,
+        /// any sequence of '\n', '\r' or ' '
+        ws_whitespace,
+
+        fn keyword(self: Token.Kind) []const u8 {
+            return switch (self) {
+                .kw_and => "and",
+                .kw_as => "as",
+                .kw_assert => "assert",
+                .kw_async => "async",
+                .kw_await => "await",
+                .kw_break => "break",
+                .kw_class => "class",
+                .kw_continue => "continue",
+                .kw_def => "def",
+                .kw_del => "del",
+                .kw_elif => "elif",
+                .kw_else => "else",
+                .kw_except => "except",
+                .kw_false => "False",
+                .kw_finally => "finally",
+                .kw_for => "for",
+                .kw_from => "from",
+                .kw_global => "global",
+                .kw_if => "if",
+                .kw_import => "import",
+                .kw_in => "in",
+                .kw_is => "is",
+                .kw_lambda => "lambda",
+                .kw_none => "None",
+                .kw_nonlocal => "nonlocal",
+                .kw_not => "not",
+                .kw_or => "or",
+                .kw_pass => "pass",
+                .kw_raise => "raise",
+                .kw_return => "return",
+                .kw_true => "True",
+                .kw_try => "try",
+                .kw_while => "while",
+                .kw_with => "with",
+                .kw_yield => "yield",
+                else => "",
+            };
+        }
+    };
+
+    /// pretty-print self onto the writer
+    pub fn debugFmt(self: Token, source: []const u8, writer: anytype) !void {
+        const text = switch (self.kind) {
+            Kind.ws_newline => "\\n",
+            else => source[self.start .. self.start + self.len],
+        };
+        try writer.print(
+            "Token(kind={s}, text=\"{s}\", start={}, len={})",
+            .{ @tagName(self.kind), text, self.start, self.len },
+        );
+    }
 };
 
 /// Tokenizer for Python code
@@ -160,23 +281,25 @@ pub const Tokenizer = struct {
     text: []const u8,
     cur: TextCursor,
 
-    pub fn new(text: []const u8) Tokenizer {
+    pub const Error = error{Eof};
+
+    pub fn init(text: []const u8) Tokenizer {
         return Tokenizer{
             .text = text,
-            .cur = TextCursor.new(text),
+            .cur = TextCursor.init(text),
         };
     }
 
-    /// Convert an identifier string to TokenKind.
-    /// This assumes the TokenKind won't be Nonsense
-    fn _identifierToTokenKind(str: []const u8) TokenKind {
+    /// Convert an identifier string to Token.Kind.
+    /// This assumes the Token.Kind won't be Nonsense
+    fn _identifierToTokenKind(str: []const u8) Token.Kind {
         assert(str.len > 0);
-        inline for (comptime std.enums.values(TokenKind)) |val| {
+        inline for (comptime std.enums.values(Token.Kind)) |val| {
             if (std.mem.eql(u8, val.keyword(), str)) {
                 return val;
             }
         }
-        return TokenKind.identifier;
+        return Token.Kind.ud_identifier;
     }
 
     fn _isIdentifierHead(c: u8) bool {
@@ -187,11 +310,11 @@ pub const Tokenizer = struct {
         return std.ascii.isAlphanumeric(c) or c == '_';
     }
 
-    pub fn next(self: *Tokenizer) TokenizerError!Token {
+    pub fn next(self: *Tokenizer) Error!Token {
         const start = self.cur.pos;
         const first_char = try self.cur.next();
 
-        // identifiers
+        // identifiers and keywords
         if (Tokenizer._isIdentifierHead(first_char)) {
             while (Tokenizer._isIdentifierTail(self.text[self.cur.pos])) {
                 self.cur.advance() catch unreachable;
@@ -199,41 +322,124 @@ pub const Tokenizer = struct {
             }
 
             const len = self.cur.pos - start;
-            return Token{ .start = start, .len = len, .kind = Tokenizer._identifierToTokenKind(self.text[start..len]) };
+            return Token{ .start = start, .len = len, .kind = Tokenizer._identifierToTokenKind(self.text[start .. start + len]) };
         }
 
-        // TODO: \n on windows
         const kind = switch (first_char) {
-            '\n' => TokenKind.ws_newline,
-            '\t' => TokenKind.ws_tab,
-            ' ' => TokenKind.ws_space,
-            else => TokenKind.nonsense,
+            // Newlines, whitespace and comments
+            '\r' => cr: {
+                const n = self.cur.curr() catch break :cr Token.Kind.ws_newline;
+                if (n == '\n') {
+                    self.cur.advance() catch unreachable;
+                    // Windows: CRLF
+                    break :cr Token.Kind.ws_newline;
+                }
+
+                // MacOS: CR
+                break :cr Token.Kind.ws_newline;
+            },
+            '\n' => Token.Kind.ws_newline, // Linux: \n
+            '\t', ' ' => Token.Kind.ws_whitespace,
+            '\\' => Token.Kind.ws_linejoin,
+            '#' => comment: {
+                // TODO: type-ignore comments
+                while (self.text[self.cur.pos] != '\n') {
+                    self.cur.advance() catch break;
+                }
+                break :comment Token.Kind.ud_comment;
+            },
+
+            // Symbols
+            '+' => plus: {
+                const v = self.cur.curr() catch break :plus Token.Kind.sm_plus;
+                if (v == '=') {
+                    self.cur.advance() catch unreachable;
+                    break :plus Token.Kind.sm_pluseq;
+                }
+                break :plus Token.Kind.sm_plus;
+            },
+
+            else => Token.Kind.ud_nonsense,
         };
-        return Token{ .start = start, .len = 1, .kind = kind };
+        return Token{ .start = start, .len = self.cur.pos - start, .kind = kind };
     }
 };
 
+test "_identifierToToken.Kind" {
+    try std.testing.expectEqual(Token.Kind.kw_while, Tokenizer._identifierToTokenKind("while"));
+    try std.testing.expectEqual(Token.Kind.kw_false, Tokenizer._identifierToTokenKind("False"));
+    try std.testing.expectEqual(Token.Kind.ud_identifier, Tokenizer._identifierToTokenKind("myId123"));
+}
+
+test "compare snapshots" {
+    var session = try SnapshotTestSession.init(std.testing.allocator, "tok");
+    defer session.deinit();
+
+    var output = try FixedBufStr.init(std.testing.allocator, SnapshotTestSession.EXPECT_CAP);
+    defer output.deinit();
+    const output_writer = output.writer();
+
+    while (try session.next()) |input| {
+        var tok = Tokenizer.init(input);
+
+        while (tok.next()) |token| {
+            try token.debugFmt(input, output_writer);
+            _ = try output_writer.write("\n");
+        } else |_| {}
+
+        try session.submitResult(output.slice());
+    }
+}
+
 test "eof" {
-    var tok = Tokenizer.new("");
-    try std.testing.expectEqual(TokenizerError.Eof, tok.next());
+    var tok = Tokenizer.init("");
+    try std.testing.expectEqual(Tokenizer.Error.Eof, tok.next());
 }
 
 test "keyword" {
-    var tok = Tokenizer.new("False");
-    try std.testing.expectEqual(Token{ .start = 0, .len = 5, .kind = TokenKind.kw_false }, tok.next());
-    try std.testing.expectEqual(TokenizerError.Eof, tok.next());
+    var tok = Tokenizer.init("False");
+    try std.testing.expectEqual(Token{ .start = 0, .len = 5, .kind = Token.Kind.kw_false }, tok.next());
+    try std.testing.expectEqual(Tokenizer.Error.Eof, tok.next());
 }
 
 test "keyword identifier" {
-    var tok = Tokenizer.new("if _test1");
-    try std.testing.expectEqual(Token{ .start = 0, .len = 2, .kind = TokenKind.kw_if }, tok.next());
-    try std.testing.expectEqual(Token{ .start = 2, .len = 1, .kind = TokenKind.ws_space }, tok.next());
-    try std.testing.expectEqual(Token{ .start = 3, .len = 6, .kind = TokenKind.identifier }, tok.next());
-    try std.testing.expectEqual(TokenizerError.Eof, tok.next());
+    var tok = Tokenizer.init("if _test1");
+    try std.testing.expectEqual(Token{ .start = 0, .len = 2, .kind = Token.Kind.kw_if }, tok.next());
+    try std.testing.expectEqual(Token{ .start = 2, .len = 1, .kind = Token.Kind.ws_whitespace }, tok.next());
+    try std.testing.expectEqual(Token{ .start = 3, .len = 6, .kind = Token.Kind.ud_identifier }, tok.next());
+    try std.testing.expectEqual(Tokenizer.Error.Eof, tok.next());
 }
 
-test "_identifierToTokenKind" {
-    try std.testing.expectEqual(TokenKind.kw_while, Tokenizer._identifierToTokenKind("while"));
-    try std.testing.expectEqual(TokenKind.kw_false, Tokenizer._identifierToTokenKind("False"));
-    try std.testing.expectEqual(TokenKind.identifier, Tokenizer._identifierToTokenKind("myId123"));
+test "identifier LF identifier" {
+    var tok = Tokenizer.init("line1\nline2");
+    try std.testing.expectEqual(Token{ .start = 0, .len = 5, .kind = Token.Kind.ud_identifier }, tok.next());
+    try std.testing.expectEqual(Token{ .start = 5, .len = 1, .kind = Token.Kind.ws_newline }, tok.next());
+    try std.testing.expectEqual(Token{ .start = 6, .len = 5, .kind = Token.Kind.ud_identifier }, tok.next());
+    try std.testing.expectEqual(Tokenizer.Error.Eof, tok.next());
+}
+
+test "identifier CRLF identifier" {
+    var tok = Tokenizer.init("line1\r\nline2");
+    try std.testing.expectEqual(Token{ .start = 0, .len = 5, .kind = Token.Kind.ud_identifier }, tok.next());
+    try std.testing.expectEqual(Token{ .start = 5, .len = 2, .kind = Token.Kind.ws_newline }, tok.next());
+    try std.testing.expectEqual(Token{ .start = 7, .len = 5, .kind = Token.Kind.ud_identifier }, tok.next());
+    try std.testing.expectEqual(Tokenizer.Error.Eof, tok.next());
+}
+
+test "identifier CR identifier" {
+    var tok = Tokenizer.init("line1\rline2");
+    try std.testing.expectEqual(Token{ .start = 0, .len = 5, .kind = Token.Kind.ud_identifier }, tok.next());
+    try std.testing.expectEqual(Token{ .start = 5, .len = 1, .kind = Token.Kind.ws_newline }, tok.next());
+    try std.testing.expectEqual(Token{ .start = 6, .len = 5, .kind = Token.Kind.ud_identifier }, tok.next());
+    try std.testing.expectEqual(Tokenizer.Error.Eof, tok.next());
+}
+
+test "comment" {
+    var tok = Tokenizer.init("line1\n# this is a comment\nline2");
+    try std.testing.expectEqual(Token{ .start = 0, .len = 5, .kind = Token.Kind.ud_identifier }, tok.next());
+    try std.testing.expectEqual(Token{ .start = 5, .len = 1, .kind = Token.Kind.ws_newline }, tok.next());
+    try std.testing.expectEqual(Token{ .start = 6, .len = 19, .kind = Token.Kind.ud_comment }, tok.next());
+    try std.testing.expectEqual(Token{ .start = 25, .len = 1, .kind = Token.Kind.ws_newline }, tok.next());
+    try std.testing.expectEqual(Token{ .start = 26, .len = 5, .kind = Token.Kind.ud_identifier }, tok.next());
+    try std.testing.expectEqual(Tokenizer.Error.Eof, tok.next());
 }
